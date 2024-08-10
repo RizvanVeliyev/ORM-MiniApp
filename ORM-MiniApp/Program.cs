6e2171ecd8aee42d0b90ed4ca86c7c473d2a9119
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-08-10 19:15:39 +0400

    Add project files.

---
+++ ORM-MiniApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ORM_MiniApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
